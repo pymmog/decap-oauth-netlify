e52590e9227c9d841d76878379ec6b0c76da5262
pymmog <pymmog@icloud.com>
2026-02-27 19:15:35 +0100

    Update OAuth redirect URL with parameters

--- netlify/functions/auth.js
+++ netlify/functions/auth.js
@@ -1,10 +1,16 @@
 exports.handler = async (event) => {
   const OAUTH_CLIENT_ID = process.env.OAUTH_CLIENT_ID;
-  const redirectUrl = `https://github.com/login/oauth/authorize?client_id=${OAUTH_CLIENT_ID}&scope=repo,user`;
+  const state = event.queryStringParameters && event.queryStringParameters.state;
+
+  const params = new URLSearchParams({
+    client_id: OAUTH_CLIENT_ID,
+    scope: "repo,user",
+    ...(state && { state }),
+  });
 
   return {
     statusCode: 302,
-    headers: { Location: redirectUrl },
+    headers: { Location: `https://github.com/login/oauth/authorize?${params}` },
     body: "",
   };
 };
